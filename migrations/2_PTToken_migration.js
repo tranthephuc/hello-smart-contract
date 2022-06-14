55769dc0693f7a0dd6e34123b807d2bc797e3aab
Phuc Tran <thephuc1990@gmail.com>
2022-06-14 15:09:27 +0700

    Added documentation related to SmartScontract Tutorial

--- migrations/2_PTToken_migration.js
+++ migrations/2_PTToken_migration.js
@@ -1,7 +1,17 @@
-// Help Truffle find `PTToken.sol` in the `/contracts` directory
-const PTToken = artifacts.require("PTToken");
+//It's used for tutorial
+// Help SimpleStorage find `PTToken.sol` in the `/contracts` directory
+const SimpleStorage = artifacts.require("SimpleStorage");
 
 module.exports = function(deployer) {
   // Command Truffle to deploy the Smart Contract
-  deployer.deploy(PTToken, 1000000);
+  deployer.deploy(SimpleStorage);
 };
+
+// It's used for PTToken
+// // Help Truffle find `PTToken.sol` in the `/contracts` directory
+// const PTToken = artifacts.require("PTToken");
+
+// module.exports = function(deployer) {
+//   // Command Truffle to deploy the Smart Contract
+//   deployer.deploy(PTToken, 1000000);
+// };
